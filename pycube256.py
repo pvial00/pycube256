6c71ba4071c0c5e1dc624649a611eb729407eeef
Karl <pvial@krypt.local>
2017-09-09 17:34:33 -0700

    pycube256 v0.2

--- pycube256.py
+++ pycube256.py
@@ -1,5 +1,4 @@
-import sys, time
-
+import hashlib
 # pycube256 0.1.1
 
 class Cube:
@@ -12,9 +11,6 @@ class Cube:
         self.start_char = 0
         self.alphabet_size = 256
         self.size_factor = 3
-        for x in range(0,self.alphabet_size):
-            self.alphabet_dict[x + self.start_char] = x
-            self.alphabet_dict_rev[x] = x + self.start_char
 
         def gen_cube(depth, width, length):
             for z in range(0,depth):
@@ -22,7 +18,7 @@ class Cube:
                 for y in range(0,width):
                     alphabet = []
                     for x in range(0,length):
-                        alphabet.append(x + self.start_char)
+                        alphabet.append(x)
                     for mod in range(0,y):
                         shift = alphabet.pop(0)
                         alphabet.append(shift)
@@ -42,22 +38,19 @@ class Cube:
     def key_cube(self, key):
         for section in self.master_list:
             for char in key:
-                char_value = char
-                sized_pos = char_value % self.size_factor
+                #sized_pos = char % self.size_factor
                 for alphabet in section:
-                    pos = alphabet.index(char)
-                    key_sub = alphabet.pop(sized_pos)
+                    key_sub = alphabet.pop(char)
                     alphabet.append(key_sub)
-                    for y in range(0,char_value):
+                    for y in range(0,char):
                         if y % 2 == 0:
                             shuffle = alphabet.pop(0)
                             alphabet.append(shuffle)
                             shuffle = alphabet.pop(2)
                             alphabet.insert(127,shuffle)
         for char in key:
-            char_value = char
-            sized_pos = char_value % self.size_factor
-            for x in range(char_value):
+            sized_pos = char % self.size_factor
+            for x in range(char):
                 section = self.master_list.pop(sized_pos)
                 newpos = (sized_pos + x) % self.size_factor
                 self.master_list.insert(newpos,section)
@@ -70,16 +63,13 @@ class Cube:
     def key_scheduler(self, key):
         sub_key = []
         for element in key:
-            pos = element
-            sized_pos = pos % self.size_factor
+            sized_pos = element % self.size_factor
             section = self.master_list.pop(sized_pos)
             sub_alpha = section.pop(sized_pos)
-            shift = sub_alpha.pop(1)
-            sub_alpha.append(shift)
-            section.insert(pos,sub_alpha)
+            sub = sub_alpha.pop(element)
+            sub_alpha.append(sub)
+            section.insert(element,sub_alpha)
             self.master_list.insert(sized_pos,section)
-            sub = sub_alpha.pop(pos)
-            sub_alpha.insert(pos,sub)
             sub_key.append(sub)
         self.load_key(sub_key)
         return sub_key
@@ -94,23 +84,22 @@ class Cube:
     def morph_cube(self, counter, sub_key):
         mod_value = counter % self.size_factor
         for key_element in sub_key:
-            key_value = key_element
-            shift_value = (mod_value + key_value) % self.size_factor
+            shift_value = (mod_value + key_element) % self.size_factor
             for section in self.master_list:
                 for alphabet in section:
                     shift = alphabet.pop(mod_value)
                     alphabet.insert(shift_value,shift)
-            section_shift = self.master_list.pop(key_value % self.size_factor)
+            section_shift = self.master_list.pop(key_element % self.size_factor)
             self.master_list.append(section_shift)
 
     def encrypt(self, words):
         cipher_text = ""
         sub_key = self.key
         for counter, letter in enumerate(words):
-            char = ord(letter)
+            sub = ord(letter)
             for section in self.master_list:
                 for alphabet in section:
-                    sub_pos = self.alphabet_dict[char]
+                    sub_pos = sub
                     sub = alphabet.pop(sub_pos)
                     alphabet.insert(sub_pos,sub)
                     shift = alphabet.pop(0)
@@ -124,14 +113,33 @@ class Cube:
         plain_text = ""
         sub_key = self.key
         for counter, letter in enumerate(words):
-            char = ord(letter)
-            for section in self.master_list:
-                for alphabet in section:
-                    sub_pos = alphabet.index(char)
-                    sub = self.alphabet_dict_rev[sub_pos]
+            sub = ord(letter)
+            for section in reversed(self.master_list):
+                for alphabet in reversed(section):
+                    sub = alphabet.index(sub)
                     shift = alphabet.pop(0)
                     alphabet.append(shift)
             sub_key = self.key_scheduler(sub_key)
             self.morph_cube(counter, sub_key)
             plain_text += chr(sub)
         return plain_text
+
+    def selftest(self):
+        hashlist = []
+        buf =""
+        result = True
+        for section in self.master_list:
+            for alphabet in section:
+                for char in alphabet:
+                    buf += chr(char)
+                entry = hashlib.sha224(buf).digest()
+                if entry in hashlist:
+                    print "Failure: Entry " + entry + " is not unique!"
+                    result = False
+                else:
+                    hashlist.append(entry)
+        if result == True:
+            print "Self test passed"
+        else:
+            print "Self test failed"
+        return result
--- scripts/cube256.py
+++ scripts/cube256.py
@@ -1,23 +1,31 @@
 from pycube256 import Cube
 import sys, select, getpass, os
-
-if select.select([sys.stdin,],[],[],0.0)[0]:
-    words = sys.stdin.read()
-else:
-    words = raw_input("Enter text to cipher: ")
-
+    
 try:
     mode = sys.argv[1]
 except IndexError as ier:
     print "Error: Did you forget encrypt/decrypt?"
     sys.exit(1)
 
-try:
-    key = sys.argv[2]
-except IndexError as ier:
-    key = getpass.getpass("Enter key: ")
+if mode == "selftest":
+    cube = Cube("ABCDEFGHIJKLMNOP")
+    cube.selftest()
+else:
 
-if mode == "encrypt":
-    print Cube(key).encrypt(words)
-elif mode == "decrypt":
-    print Cube(key).decrypt(words)
+    if select.select([sys.stdin,],[],[],0.0)[0]:
+        words = sys.stdin.read()
+        lastbyte = words[len(words) - 1:len(words)]
+        if lastbyte == chr(10):
+            words = words[:len(words) - 1]
+    else:
+        words = raw_input("Enter text to cipher: ")
+
+    try:
+        key = sys.argv[2]
+    except IndexError as ier:
+        key = getpass.getpass("Enter key: ")
+
+    if mode == "encrypt":
+        print Cube(key).encrypt(words)
+    elif mode == "decrypt":
+        print Cube(key).decrypt(words)
