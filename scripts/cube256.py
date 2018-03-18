b17e0e39b0483ce7f27521c83a28a558c328a429
Karl <pvial@krypt.local>
2018-03-17 19:25:35 -0700

    file encryption script

--- scripts/cube256.py
+++ scripts/cube256.py
@@ -1,35 +1,51 @@
-from pycube256 import Cube
+from pycube256 import Cube, CubeRandom, CubeKDF
 import sys, select, getpass, os, time, getopt
-    
+
 try:
     mode = sys.argv[1]
 except IndexError as ier:
     print "Error: Did you forget encrypt/decrypt?"
     sys.exit(1)
 
-if mode == "selftest":
-    cube = Cube("ABCDEFGHIJKLMNOP")
-    cube.selftest()
-else:
+input_filename = sys.argv[2]
+output_filename = sys.argv[3]
 
-    if select.select([sys.stdin,],[],[],0.0)[0]:
-        data = sys.stdin.read()
-    else:
-        sys.exit(1)
+try:
+    infile = open(input_filename, "r")
+except IOError as ier:
+    print "Input file not found."
+    sys.exit(1)
 
-    try:
-        key = sys.argv[2]
-    except IndexError as ier:
-        key = getpass.getpass("Enter key: ")
+try:
+    outfile = open(output_filename, "w")
+except IOError as ier:
+    print "Output file not found."
+    sys.exit(1)
 
-    start = time.time()
+try:
+    key = sys.argv[4]
+except IndexError as ier:
+    key = getpass.getpass("Enter key: ")
 
-    if mode == "encrypt":
-        sys.stdout.write(Cube(key).encrypt(data))
-    elif mode == "decrypt":
-        sys.stdout.write(Cube(key).decrypt(data))
+# 128 bit key size
+key = CubeKDF().genkey(key, length=16)
 
-    end = time.time() - start
-    bps = len(data) / end
-    sys.stderr.write("Completed in "+str(end)+" seconds\n")
-    sys.stderr.write(str(bps)+" bytes per second.\n")
+start = time.time()
+data = infile.read()
+infile.close()
+
+if mode == "encrypt":
+    nonce = os.urandom(16)
+    c = Cube(key, nonce).encrypt(data)
+    outfile.write(nonce+c)
+elif mode == "decrypt":
+    nonce = data[:16]
+    msg = data[16:len(data) -1]
+    plain_text = Cube(key, nonce).decrypt(msg)
+    outfile.write(plain_text)
+outfile.close()
+
+end = time.time() - start
+bps = len(data) / end
+sys.stdout.write("Completed in "+str(end)+" seconds\n")
+sys.stdout.write(str(bps)+" bytes per second.\n")
